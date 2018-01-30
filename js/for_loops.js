45aaa7545e97a173364f77cdce851087800afe04
tonygarza <garzaan84@gmail.com>
2018-01-30 16:02:51 -0600

    loops.html/while.js

---
+++ js/for_loops.js
@@ -0,0 +1,3 @@
+"use strict";
+
+someFunction ("showMultiplicationTable");
--- js/while.js
+++ js/while.js
@@ -1,19 +1,31 @@
 "use strict";
 
-var num =2;
-var i = 1;
 
-while (i<16){
+    var i = 1;
 
-    console.log (num);
-    num *=2;
-    i++;
+    while (i <= 10) {
+        console.log('while loop iteration #' + i);
+        i++;
+    }
 
+    i = 10;
 
+    while (i <= 20) {
+        console.log('while loop iteration #' + i);
+        i++;
+    }
 
+    i = 20;
 
+    while (i >= 0) {
+        console.log('while loop iteration #' + i);
+        i = i - 1;
+    }
 
-
+for (var i = 0, j = 9; i < 10; i++, j--) {
+    console.log('for loop iteration #' + i + ', j = ' + j);
 }
+
+
 
 
